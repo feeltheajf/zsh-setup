d77a4103dad1eadc001e21c8252f8f2e695c6824
feeltheajf <radostev.ilya4955148@gmail.com>
2018-05-05 22:54:55 +0300

    initial commit

---
+++ install.sh
@@ -0,0 +1,35 @@
+echo "Hi, ${USER}! I will set up ZSH for you"
+
+prefix=$(dirname ${0})
+
+package_manager="apt-get install -y --upgrade"
+
+if [ -f "/usr/bin/pacman" ]; then
+	package_manager="pacman -Sy --noconfirm"
+fi
+
+echo "I need root privileges to install necessary packages"
+sudo -K
+sh -c "sudo ${package_manager} zsh &> /dev/null" 
+
+echo "Setting up Oh-My-ZSH"
+rm -rf ~/.oh-my-zsh
+git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh &> /dev/null
+
+echo "Configuring plugins"
+mkdir -p .oh-my-zsh/custom/plugins
+cp ${prefix}/user/nanorc            ~/.nanorc
+cp ${prefix}/user/zshrc             ~/.zshrc
+cp ${prefix}/user/my.zsh-theme      ~/.oh-my-zsh/themes/my.zsh-theme
+cp ${prefix}/user/zsh-peco-history   ~/.oh-my-zsh/custom/plugins/zsh-peco-history -R
+sudo cp ${prefix}/bin/peco          /usr/local/bin/peco
+
+sh -c "cd ${prefix}/bin/fasd && sudo make install &> /dev/null"
+sh -c "mkdir -p ~/.config/peco && cp ${prefix}/user/config.json ~/.config/peco/ && peco --rcfile ~/.config/peco/config.json &> /dev/null"
+
+chsh ${USER} -s /usr/bin/zsh
+
+echo "Setup completed"
+echo "Bye, ${USER}!"
+
+exec -l /usr/bin/zsh
