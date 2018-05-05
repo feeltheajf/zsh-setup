adb8c3287c39f8f9b9ef377986228bac0eb7558b
feeltheajf <radostev.ilya4955148@gmail.com>
2018-05-05 13:07:44 -0700

    initial commit

--- install.sh
+++ install.sh
@@ -10,11 +10,11 @@ fi
 
 echo "I need root privileges to install necessary packages"
 sudo -K
-sh -c "sudo ${package_manager} zsh &> /dev/null" 
+sh -c "sudo ${package_manager} zsh "
 
 echo "Setting up Oh-My-ZSH"
 rm -rf ~/.oh-my-zsh
-git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh &> /dev/null
+git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 echo "Configuring plugins"
 mkdir -p .oh-my-zsh/custom/plugins
@@ -24,12 +24,12 @@ cp ${prefix}/user/my.zsh-theme      ~/.oh-my-zsh/themes/my.zsh-theme
 cp ${prefix}/user/zsh-peco-history   ~/.oh-my-zsh/custom/plugins/zsh-peco-history -R
 sudo cp ${prefix}/bin/peco          /usr/local/bin/peco
 
-sh -c "cd ${prefix}/bin/fasd && sudo make install &> /dev/null"
-sh -c "mkdir -p ~/.config/peco && cp ${prefix}/user/config.json ~/.config/peco/ && peco --rcfile ~/.config/peco/config.json &> /dev/null"
+sh -c "cd ${prefix}/bin/fasd && sudo make install"
+sh -c "mkdir -p ~/.config/peco && cp ${prefix}/user/config.json ~/.config/peco/ && peco --rcfile ~/.config/peco/config.json 2> /dev/null"
 
 chsh ${USER} -s /usr/bin/zsh
 
 echo "Setup completed"
 echo "Bye, ${USER}!"
 
-exec -l /usr/bin/zsh
+exec -l zsh
